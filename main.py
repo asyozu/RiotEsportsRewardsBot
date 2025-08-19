a707a99052b5d02ffaf68a2de53d1d191d1f797e
asyozu <165659537+asyozu@users.noreply.github.com>
2025-08-19 16:34:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def send_rewards(session, token, body):
     async with session.post(REWARDS_URL, headers=headers, json=body) as response:
         response_data = await response.json()
         print(f"Received: {response_data}")
-async def process_event(event, session):
+async def process_event(event, session, current_time):
     if event['type'] == 'match':
         teams = event.get('match', {}).get('teams', [])
         print(f"Currently broadcasting {event['league']['name']} Match: {event['blockName']} - {teams[0]['name']} ({teams[0]['code']}) vs. {teams[1]['name']} ({teams[1]['code']})")
@@ -34,7 +34,7 @@ async def process_event(event, session):
     body = {
         'stream_id': stream['parameter'],
         'source': stream['provider'],
-        'stream_position_time': datetime.now(timezone.utc).isoformat(),
+        'stream_position_time': current_time,
         'geolocation': {'code': 'VN', 'area': 'AS'},
         'tournament_id': tournament_id,
     }
@@ -51,7 +51,8 @@ async def main():
                     print('No live events found.')
                     await asyncio.sleep(60)
                     continue
-                await asyncio.gather(*(process_event(event, session) for event in events))
+                current_time = datetime.now(timezone.utc).isoformat()
+                await asyncio.gather(*(process_event(event, session, current_time) for event in events))
             except Exception as e:
                 print(f'Error: {e}')
             await asyncio.sleep(60)
